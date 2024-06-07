29a4b64288b52691d2b1089795e11b864671d5d0
Aladser <aladser@gmail.com>
2024-06-07 23:21:03 +0900

    обновлено верхнее меню индексной страницы

---
+++ local/templates/index/footer.php
@@ -0,0 +1,236 @@
+<div class="sl_slider" id="slides">
+			<div class="slides_container" style="overflow: hidden; position: relative; display: block;">
+				<div class="slides_control" style="position: relative; width: 8955px; height: 270px; left: -2985px;"><div class="slides_control" style="position: relative; width: 8955px; height: 270px; left: -2985px; top: 0px; z-index: 5; display: block; opacity: 0.801107;"><div style="position: absolute; top: 0px; left: 2985px; z-index: 5; height: auto; display: none;">
+					<div>
+						<img src="content/1.jpg?1717765110304?1717769226335" alt="">
+						<h2><a href="">Диваны и кресла</a></h2>
+						<p>Новая комбинация для ТВ БЕСТО не просто предмет мебели – она разработана, также, для мультимедиа. Провода и сетевые кабели можно хранить внутри комбинации или протянуть через отверстие для вентиляции. Секция полок обеспечивает эффективное использование стены, освобождая место на полу.</p>
+						<a href="" class="sl_more">Подробнее →</a>
+					</div>
+				</div><div style="position: absolute; top: 0px; left: 2985px; z-index: 0; height: auto; display: none;">
+					<div>
+						<img src="content/1.jpg" alt="">
+						<h2><a href="">Диваны и кресла</a></h2>
+						<p>Новая комбинация для ТВ БЕСТО не просто предмет мебели – она разработана, также, для мультимедиа. Провода и сетевые кабели можно хранить внутри комбинации или протянуть через отверстие для вентиляции. Секция полок обеспечивает эффективное использование стены, освобождая место на полу.</p>
+						<a href="" class="sl_more">Подробнее →</a>
+					</div>
+				</div></div></div>
+				
+			</div>
+		<ul class="pagination"><li class="current"><a href="#0">1</a></li><li class=""><a href="#1">2</a></li><li class=""><a href="#0">1</a></li><li><a href="#1">2</a></li><li><a href="#2">3</a></li></ul><ul class="pagination"><li><a href="#0">1</a></li><li><a href="#1">2</a></li><li><a href="#2">3</a></li></ul></div>
+
+		
+		<!--- // end slider area --->
+		
+		<div class="main_container homepage">
+			
+			<!-- events -->
+			<div class="ev_events">
+				<div class="ev_h">
+					<h3>Ближайшие события</h3>
+					<a href="" class="ev_allevents">Все мероприятия →</a>
+				</div>
+				<ul class="ev_lastevent">
+					<li>
+						<h4><a href="">29 августа 2012, Москва</a></h4>
+						<p>Семинар производителей мебели России и СНГ, Обсуждение тенденций.</p>
+					</li>
+					<li>
+						<h4><a href="">30 августа 2012, Санкт-Петербург</a></h4>
+						<p>Открытие шоу-рума на Невском проспекте. Последние модели в большом ассортименте.</p>
+					</li>
+					<li>
+						<h4><a href="">31 августа 2012, Краснодар</a></h4>
+						<p>Открытие нового магазина в нашей дилерской сети.</p>
+					</li>
+				</ul>
+				<div class="clearboth"></div>
+			</div>
+			<!-- // end events -->
+			<div class="cn_hp_content">
+				<div class="cn_hp_category">
+					<ul>
+						<li>
+							<img src="content/1.png" alt="">
+							<h2><a href="">Мягкая мебель</a></h2>
+							<p>Диваны, кресла и прочая мягкая мебель <a class="cn_hp_categorymore" href="">→</a></p>
+							<div class="clearboth"></div>
+						</li>
+						<li>
+							<img src="content/2.png" alt="">
+							<h2><a href="">Офисная мебель</a></h2>
+							<p>Диваны, столы, стулья <a class="cn_hp_categorymore" href="">→</a></p>
+							<div class="clearboth"></div>
+						</li>
+						<li>
+							<img src="content/3.png" alt="">
+							<h2><a href="">Мебель для кухни</a></h2>
+							<p>Полки, ящики, столы и стулья <a class="cn_hp_categorymore" href="">→</a></p>
+							<div class="clearboth"></div>
+						</li>
+						<li>
+							<img src="content/4.png" alt="">
+							<h2><a href="">Детская мебель</a></h2>
+							<p>Кровати, стулья, мягкая детская мебель <a class="cn_hp_categorymore" href="">→</a></p>
+							<div class="clearboth"></div>
+						</li>
+					</ul>
+					<a href="" class="cn_hp_category_more">Все разделы каталога →</a>
+				</div>
+				<div class="cn_hp_post">
+					<div class="cn_hp_post_new">
+						<h3>Новинки</h3>
+						<img src="content/7.png" alt="">
+						<p>Угловой диван "Титаник", с большим выбором расцветок и фактур.</p>
+						<div class="clearboth"></div>
+					</div>
+					<div class="cn_hp_post_action">
+						<h3>Акции</h3>
+						<img src="content/7.png" alt="">
+						<p>Угловой диван "Титаник", с большим выбором расцветок и фактур.</p>
+						<div class="clearboth"></div>
+					</div>
+					<div class="cn_hp_post_bestsellersn">
+						<h3>Хиты продаж</h3>
+						<img src="content/7.png" alt="">
+						<p>Угловой диван "Титаник", с большим выбором расцветок и фактур.</p>
+						<div class="clearboth"></div>
+					</div>
+				</div>
+				<div class="cn_hp_lastnews">
+					<h3><a href="">Новости</a></h3>		
+					<ul>
+						<li>
+							<h4><a href="">29 августа 2012</a></h4>
+							<p>Поступление лучших офисных кресел из Германии</p>
+						</li>
+						<li>
+							<h4><a href="">29 августа 2012</a></h4>
+							<p>Мастер-класс дизайнеров из Италии для производителей мебели</p>
+						</li>
+						<li>
+							<h4><a href="">29 августа 2012</a></h4>
+							<p>Открытие нашего нового офиса рядом с метро Измайлово</p>
+						</li>
+						<li>
+							<h4><a href="">29 августа 2012</a></h4>
+							<p>Наша дилерская сеть расширилась теперь ассортимент наших товаров доступен в Казани</p>
+						</li>					
+					</ul>
+					<br>
+					<a href="" class="cn_hp_lastnews_more">Все новости →</a>
+				</div>
+				<div class="clearboth"></div>
+			</div>
+		</div>
+		
+		<script type="text/javascript">
+			$(document).ready(function(){
+			
+				$("#foo").carouFredSel({
+					items:2,
+					prev:'#rwprev',
+					next:'#rwnext',
+					scroll:{
+						items:1,
+						duration:2000
+					}
+				});	
+			});	
+		</script>
+	
+		<div class="rw_reviewed">
+			<div class="rw_slider">
+				<h4>Отзывы</h4>
+				<div class="caroufredsel_wrapper" style="display: block; text-align: start; float: none; position: relative; inset: auto; z-index: auto; width: 1050px; height: 216px; margin: 0px; overflow: hidden;"><div class="caroufredsel_wrapper" style="display: block; text-align: left; float: none; position: absolute; inset: 0px -3210px -50px 0px; z-index: auto; width: 1050px; height: 217px; margin: 0px; overflow: hidden;"><ul id="foo" style="text-align: left; float: none; position: absolute; inset: 0px auto auto 0px; margin: 0px; width: 4200px; height: 217px; z-index: auto;">
+					
+					
+					
+				<li>
+						<div class="rw_message">
+							<img src="content/8.png" class="rw_avatar" alt="">
+							<span class="rw_name">Дмитрий Иванов</span>
+							<span class="rw_job">Генеральный директор группы компаний "Офис+"</span>
+							<p>“В магзине предоставили потрясающий выбор расцветок, а также, получил большую скидку по карте постоянного клиента.”</p>
+							<div class="clearboth"></div>
+							<div class="rw_arrow"></div>
+						</div>
+					</li><li>
+						<div class="rw_message">
+							<img src="content/8.png" class="rw_avatar" alt="">
+							<span class="rw_name">Сергей Антонов</span>
+							<span class="rw_job">Руководитель финансового отдела “Банк+”</span>
+							<p>“Покупал офисные стулья и столы, остался очень доволен! Низкие цены, быстрая доставка, обслуживание на высоте! Спасибо!”</p>
+							<div class="clearboth"></div>
+							<div class="rw_arrow"></div>
+						</div>
+					</li><li>
+						<div class="rw_message">
+							<img src="content/8.png" class="rw_avatar" alt="">
+							<span class="rw_name">Сергей Антонов</span>
+							<span class="rw_job">Руководитель финансового отдела “Банк+”</span>
+							<p>“Покупал офисные стулья и столы, остался очень доволен! Низкие цены, быстрая доставка, обслуживание на высоте! Спасибо!”</p>
+							<div class="clearboth"></div>
+							<div class="rw_arrow"></div>
+						</div>
+					</li></ul></div></div>
+				<div id="rwprev" class="" style="display: block;"></div>
+				<div id="rwnext" class="" style="display: block;"></div>
+				<a href="" class="rw_allreviewed">Все отзывы</a>
+			</div>
+		</div>
+		
+		<div class="ft_footer">
+			<div class="ft_container">
+				<div class="ft_about">
+					<h4>О магазине</h4>
+					<ul>
+						<li><a href="">Отзывы</a></li>
+						<li><a href="">Контакты</a></li>
+						<li><a href="">Руководство</a></li>
+						<li><a href="">История</a></li>
+					</ul>
+				</div>
+				<div class="ft_catalog">
+					<h4>Каталог товаров</h4>
+					<ul>
+						<li><a href="">Кухни</a></li>
+						<li><a href="">Кровати и кушетки</a></li>
+						<li><a href="">Гарнитуры</a></li>
+						<li><a href="">Тумобчки и прихожие</a></li>
+						<li><a href="">Спальни и матрасы</a></li>
+						<li><a href="">Аксессуары</a></li>
+						<li><a href="">Столы и стулья</a></li>
+						<li><a href="">Каталоги мебели</a></li>
+						<li><a href="">Раскладные диваны</a></li>
+						<li><a href="">Кресла</a></li>
+					</ul>
+					
+				</div>
+				<div class="ft_contacts">
+					<h4>Контактная информация</h4>
+					<!-- vCard        http://help.yandex.ru/webmaster/hcard.pdf      -->
+					<p class="vcard">
+						<span class="adr">
+							<span class="street-address">ул. Летняя стр.12, офис 512</span>
+						</span>
+						<span class="tel">8 (495) 212-85-06</span>
+						<strong>Время работы:</strong> <br> <span class="workhours">ежедневно с 9-00 до 18-00</span><br>
+					</p>
+					<ul class="ft_solcial">
+						<li><a href="" class="fb"></a></li>
+						<li><a href="" class="tw"></a></li>
+						<li><a href="" class="ok"></a></li>
+						<li><a href="" class="vk"></a></li>
+					</ul>
+					<div class="ft_copyright">©&nbsp;2000&nbsp;-&nbsp;2012 "Мебельный магазин" </div>
+					
+					
+				</div>
+				
+				<div class="clearboth"></div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
--- test.php
+++ test.php
@@ -1,22 +1,4 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Тест");
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:menu", 
-	"main_menu", 
-	array(
-		"ALLOW_MULTI_SELECT" => "N",
-		"CHILD_MENU_TYPE" => "left",
-		"DELAY" => "N",
-		"MAX_LEVEL" => "2",
-		"MENU_CACHE_GET_VARS" => array(
-		),
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "N",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"ROOT_MENU_TYPE" => "top",
-		"USE_EXT" => "N",
-		"COMPONENT_TEMPLATE" => "main_menu"
-	),
-	false
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
