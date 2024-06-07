5a1087767509962f830cd01caa84cd4ed957fc2f
Aladser <aladser@gmail.com>
2024-06-08 06:58:32 +0900

    up

--- test.php
+++ test.php
@@ -1,4 +1,13 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Тест");
-?>111<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:system.auth.form",
+	"",
+	Array(
+		"FORGOT_PASSWORD_URL" => "",
+		"PROFILE_URL" => "",
+		"REGISTER_URL" => "",
+		"SHOW_ERRORS" => "N"
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
