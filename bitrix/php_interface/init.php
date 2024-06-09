9306e6fe2d1d44296c705f46b38539401b0d52a1
Aladser <aladser@gmail.com>
2024-06-09 09:59:29 +0900

    обновление верстки

---
+++ bitrix/php_interface/init.php
@@ -0,0 +1,8 @@
+<?
+    function dump($value) {
+        echo '<pre>';
+        var_dump($value);
+        echo '</pre>';
+    }
+
+    define('TEMPLATE_FOLDER', '/local/templates/.default');
--- test.php
+++ test.php
@@ -1,12 +1,4 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Тест");
-?><?$APPLICATION->IncludeComponent("bitrix:system.auth.form", "auth_component", Array(
-	"FORGOT_PASSWORD_URL" => "/user/",	// Страница забытого пароля
-		"PROFILE_URL" => "/user/profile.php",	// Страница профиля
-		"REGISTER_URL" => "/user/register.php",	// Страница регистрации
-		"SHOW_ERRORS" => "Y",	// Показывать ошибки
-		"COMPONENT_TEMPLATE" => ".default"
-	),
-	false
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
